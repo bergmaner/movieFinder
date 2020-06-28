e7549a574200f045769fa797febd4848110fa8dc
bergmaner <bergkacper@gmail.com>
2020-06-28 20:06:20 +0200

    Updates

---
+++ precache-manifest.efe40b7ae359c03788f6ffe40e89940b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dbf8a943e6d5c1a34a4c494a9ca675b",
+    "url": "/movieFinder/index.html"
+  },
+  {
+    "revision": "45c2e4b2af813c0669fe",
+    "url": "/movieFinder/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "51329357066fd6c6ea07",
+    "url": "/movieFinder/static/js/2.cb93066e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movieFinder/static/js/2.cb93066e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45c2e4b2af813c0669fe",
+    "url": "/movieFinder/static/js/main.a5339959.chunk.js"
+  },
+  {
+    "revision": "102ba3b7757dba187bf6",
+    "url": "/movieFinder/static/js/runtime-main.82518eb9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movieFinder/static/media/logo.5d5d9eef.svg"
+  }
+]);
